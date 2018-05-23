94d25754bd66628a719affece590a0e0312a6e35
Kevin Wu <kevin.wu@live.com.my>
2018-05-23 16:18:09 +0800

    react base project setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+require('dotenv').config();
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: ['whatwg-fetch', './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3031,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify(process.env.API_URL),
+    }),
+  ],
+};
